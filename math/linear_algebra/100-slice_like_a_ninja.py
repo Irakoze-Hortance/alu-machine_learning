a3e4de07a439cb09062cff8ad60428007a1bdcaa
Irakoze-Hortance <hortanceirakoze@gmail.com>
2025-05-22 13:58:01 +0200

    advanced tasks

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python3
-"""
-Slice numpy arrays
-"""
-import numpy as np
-
-
-def np_slice(matrix, axes=None):
+def np_slice(matrix, axes={}):
     """Slices a matrix along specific axes
 
     Args:
@@ -17,8 +10,6 @@ def np_slice(matrix, axes=None):
     Returns:
         numpy.ndarray: the sliced matrix
     """
-    if axes is None:
-        return matrix[...]
     slices = []
     for i in range(matrix.ndim):
         if i in axes:
@@ -26,12 +17,3 @@ def np_slice(matrix, axes=None):
         else:
             slices.append(slice(None))
     return matrix[tuple(slices)]
-  
-mat1 = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-print(np_slice(mat1, axes={1: (1, 3)}))
-print(mat1)
-# mat2 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-#                  [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
-#                  [[21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]])
-# print(np_slice(mat2, axes={0: (2,), 2: (None, None, -2)}))
-# print(mat2)
